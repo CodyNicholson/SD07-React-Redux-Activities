66e29469a2d669cac1a1d8446b56e165aeffffcc
Cody Nicholson <codynicholson96@gmail.com>
2023-04-05 16:59:46 -0500

    fix

--- D10/2-RR-React-Redux-Thunk-Art-Gallery-START/src/App.js
+++ D10/2-RR-React-Redux-Thunk-Art-Gallery-START/src/App.js
@@ -1,33 +1,20 @@
 import './App.css';
-import { useSelector, useDispatch } from 'react-redux';
-import {clearData, fetchData, inputId, incrementId, decrementId} from './features/dataSlice';
+import { useSelector, useDispatch } from 'react-redux'
 
 function App() {
-  const dispatch = useDispatch();
-  const data = useSelector(state => state.data);
-
-  const renderImg = () => {
-    if(data.apiData) {
-      return <img style={{'width': '100vw'}} src={data.apiData.primaryImage} alt={data.apiData.title} />
-    } else {
-      return <p>No image</p>
-    }
-  }
+  // your logic goes here!
 
   return (
     <div className="App">
       <div>
-        <button onClick={() => dispatch(fetchData())}>Trigger Thunk</button>
-        <button onClick={() => dispatch(clearData())}>Clear</button>
-        <button onClick={() => dispatch(incrementId())}>Next</button>
-        <button onClick={() => dispatch(decrementId())}>Back</button>
+        <button onClick={() => {}}>Trigger Thunk</button>
+        <button onClick={() => {}}>Clear</button>
+        <button onClick={() => {}}>Next</button>
+        <button onClick={() => {}}>Back</button>
       </div>
-      <input value={data.objectId} onChange={(e) => {
-        dispatch(inputId(Number(e.target.value)))
-      }} />
+      <input onChange={(e) => { }} />
       <div>
-        <h1>{data.objectId}</h1>
-        {renderImg()}
+        {/* Once you have plugged everything in, render the image here! */}
       </div>
     </div>
   );
--- D10/2-RR-React-Redux-Thunk-Art-Gallery-START/src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import dataReducer from './features/dataSlice';
-
-export const store = configureStore({
-    reducer: {
-        data: dataReducer
-    }
-})
